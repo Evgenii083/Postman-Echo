e2dfc0af95d2b6b78d2cf7ba97cae8acd27336e3
Evgenii Rybalkin <eugeny.rybalkin@gmail.com>
2023-01-07 20:31:31 +0100

    expected successes build

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -14,7 +14,7 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo(" information"))
+                .body("data", equalTo("Test message"))
         ;
     }
 }
